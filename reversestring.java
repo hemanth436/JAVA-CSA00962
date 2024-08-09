31292e47263f8c68ca36e162f0c2d37632f65084
hemanth436 <hemanthreddym2098.sse@saveetha.com>
2024-08-09 15:00:30 +0530

    Add files via upload

---
+++ reversestring.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class reverse{
+    public static void main(String args[])
+    {
+        String s="hemanth";
+        for(int i=s.length();i>0;i--)
+        {
+            System.out.println("revesr string is "+i);
+        }
+    }
+}
